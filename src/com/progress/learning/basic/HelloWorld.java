c66568e05c4816f39b812626b11d050fb21c39a6
ice-bear <andydowsen47us@gmail.com>
2022-11-30 13:22:28 +0530

    Append hello world program, in learning module

---
+++ src/com/progress/learning/basic/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * @title: Hello world program
+ * @author: i-ice-bear, Anshu Naagar
+ * */
+
+package com.progress.learning.basic;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
