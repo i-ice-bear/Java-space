808d17ce6fa539cb9e7a4656ef0a37eeec0e3100
ice-bear <andydowsen47us@gmail.com>
2022-12-01 22:03:49 +0530

    Append bit calculator in algorithm base

--- src/algorithms/calculations/bitwiseCalculator.java
+++ src/algorithms/calculations/bitwiseCalculator.java
@@ -1,7 +1,19 @@
 package algorithms.calculations;
 
+import java.util.Scanner;
+
 public class bitwiseCalculator {
     public static void main(String[] args) {
-        System.out.println("bitwise calculator");
+        System.out.println("Bitwise calculator ! ");
+        Scanner enterBitwise = new Scanner(System.in);
+
+        System.out.print("Enter first modulo : ");
+        float bitwiseCalculations = enterBitwise.nextByte();
+
+        System.out.print("Enter second modulo : ");
+        float bitwiseVariable2 = enterBitwise.nextByte();
+
+        float outputBitwiseCalculation = (bitwiseCalculations % bitwiseVariable2);
+        System.out.println("Output Bitwise Calculation : " + outputBitwiseCalculation);
     }
 }
