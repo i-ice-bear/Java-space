d5fd49dcffa06207c821b2b8fd47423ebdcf671f
ice-bear <andydowsen47us@gmail.com>
2022-12-02 16:16:03 +0530

    Append incrementTypeOperationExecution in learning space

---
+++ src/com/progress/learning/basic/incrementTypeOperator.java
@@ -0,0 +1,20 @@
+package com.progress.learning.basic;
+
+public class incrementTypeOperator {
+    public static void main(String[] args) {
+
+        // Increment and Decrement Operators
+        int i = 56;
+        // int b = i++; // first b is assigned i (56) then i is incremented
+        int j = 67;
+        int c = ++j; // first j is incremented then c is assigned j (68)
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+        int y = 7;
+        System.out.println( ++y *8);
+        char ch = 'a';
+        System.out.println(++ch);
+    }
+}
