e82f71996a72b719e76256b9c057f6f83183cafb
ice-bear <andydowsen47us@gmail.com>
2022-12-07 18:13:27 +0530

    Reconfigured starSet in problem base

--- problems/src/questionSets/starSet.java
+++ problems/src/questionSets/starSet.java
@@ -5,9 +5,9 @@ public class starSet {
         int rows, columns;
         rows = 4;
         columns = 6;
-        for (int i = 0; i < columns; i++) {
-            for (int j = 0; j < rows; j++) {
-                System.out.println('*');
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print('*');
             }
             System.out.println('*');
         }
