c90f56504209507e6217b9d2d2087320c4a9211d
ice-bear <andydowsen47us@gmail.com>
2022-12-21 15:06:42 +0530

    Append method overloading backlogs

--- src/com/progress/learning/basic/Chapter7/methodOverloading.java
+++ src/com/progress/learning/basic/Chapter7/methodOverloading.java
@@ -1,20 +1,56 @@
 package com.progress.learning.basic.Chapter7;
-import java.util.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
 
 public class methodOverloading {
-	static void change(int takeArgument) {
-		takeArgument = 30;
-		System.out.println("Argument is : " + takeArgument);
+	static void change(int integer){
+		integer = 3232;
+		System.out.println("integer variable from change is : " + integer);
 	}
+	static void tellJoke(){
+		System.out.println("I make a new world \n" + "Plagiarism");
+	}
+	void addTwoNumbers (int _number_arg_1, int _number_arg_2){
+		int output = _number_arg_1 + _number_arg_2;
+		System.out.println("\t The output is : " + output);
+	}
+
+	/* * Method overloading starts form there */
+	static void goodMorning(){
+		System.out.println("Good morning, anything ");
+	}
+	static void goodMorning(int a){
+		System.out.println("Good morning " + a + " Bro");
+	}
+	static void goodMorning(int a, int b){
+		System.out.println("Good morning " + a + " Bro " + b);
+	}
+	static void goodMorning(String[] getName){
+		System.out.println("Good morning " + Arrays.toString(getName));
+	}
+	static void changeArray(int @NotNull [] arrayPosition){
+		arrayPosition[0] = 902;
+		System.out.println("The 0th element is : " + arrayPosition[0]);
+	}
+
 	public static void main(String[] args) {
-		int x = 302;
-		change(x);
-		int [] elementChange = {2342, 12432, 23423, 2134};
-		change2(elementChange);
-		System.out.println("The element is : " + elementChange[0]);
-	}
-	static void change2(int [] arrayChange){
-		arrayChange[0] = 232;
-		System.out.println("the element of change 2 is : " + arrayChange[0]);
-	}
+		tellJoke();
+		int xVariable = 32;
+		methodOverloading unsharedMethods = new methodOverloading();
+		unsharedMethods.addTwoNumbers(3, 9);
+		// * Case 1 : Method changeability inside the changeFunction.
+		System.out.println("X variable is : " + xVariable);
+		change(xVariable);
+
+		// * case 2 : Array changeability.
+		int [] arrayGet = {12, 32, 234, 523};
+		changeArray(arrayGet);
+
+		// * case 3 : Method overloading
+		goodMorning();
+		goodMorning(300); // * Arguments are actual
+		String[] goodName = { "anshu" };
+		goodMorning(goodName);
+ 	}
 }
